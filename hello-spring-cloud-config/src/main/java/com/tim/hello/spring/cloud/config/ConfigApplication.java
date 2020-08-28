878efcb14ddb6ecc9af320f3513046d51685ac74
timwang <kevintimwang@gmail.com>
2020-08-28 17:28:47 +0800

    更新配置中心

---
+++ hello-spring-cloud-config/src/main/java/com/tim/hello/spring/cloud/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.tim.hello.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ Author  : Tim Wang
+ * @ FileName: ConfigApplication.java
+ * @ Time    : 2020/8/27 19:24
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
